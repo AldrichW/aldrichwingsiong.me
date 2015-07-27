47262bbb5266abc3d44a757ef0c42cb12843557c
Aldrich Wingsiong <aldrich.wingsiong@gmail.com>
2015-07-27 00:08:42 -0400

    Changing the main banner to utilize the screen space more and have a more open and full screen theme to my site

---
+++ components/navBar/navBar.controller.js
@@ -0,0 +1,6 @@
+var PersonalWebApp = angular.module("PersonalWebApp");
+
+PersonalWebApp.controller("ScrollControl", ['$scope','$anchorScroll',
+     function($scope,$anchorScroll){
+         
+     }]);
